a083638cc21707c474b894afdb6db23fa29e1143
dengwenj <927561237@qq.com>
2022-03-12 15:40:15 +0800

    用户注册接口搭建

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,14 @@
 import Koa from 'koa'
+import Router from 'koa-router'
 
 const app = new Koa()
+const userRouter = new Router({ prefix: '/user' })
+
+userRouter.post('/', (ctx, next) => {
+  ctx.body = '创建用户成功'
+})
+
+app.use(userRouter.routes())
+app.use(userRouter.allowedMethods())
 
 export default app
