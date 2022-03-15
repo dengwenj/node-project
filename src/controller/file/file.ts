6d72ced87ffd3783d8e7e06e8822a7a0efb6453e
dengwenj <927561237@qq.com>
2022-03-16 00:06:11 +0800

    把头像数信息存到数据库，在这之前要判断下 avatar 表中有没有存储该用户的头像信息，没有存储的话才是插入，有的话更新

--- src/controller/file/file.ts
+++ src/controller/file/file.ts
@@ -8,7 +8,6 @@ import type { Context, Next } from "koa"
 import type { IIncomingMessage } from './types'
 
 
-
 class fileController {
   // 上传头像
   async saveAvatarInfo(ctx: Context, next: Next) {
@@ -19,8 +18,16 @@ class fileController {
     if (req.file) {
       const { filename, size, mimetype } = req.file
 
-      // 吧数据保存到数据库
-      const res = await fileservice.avatarInfo(filename, size, mimetype, userId)
+      // 把头像数信息存到数据库，在这之前要判断下 avatar 表中有没有存储该用户的头像信息，没有存储的话才是插入，有的话更新
+      const isAvatarInfo = await fileservice.isAvatarInfo(userId)
+      if (!isAvatarInfo) {
+        // 插入
+        const res = await fileservice.avatarInfo(filename, size, mimetype, userId)
+      } else {
+        // 更新
+        const res = await fileservice.updateAvatarInfo(filename, size, mimetype, userId)
+      }
+      
       // 把获取头像路径存放在 user 表里
       const avatarUrl = `${env.APP_HOST}:${env.APP_PORT}/user/${userId}/avatar`
       const res1 = await userserice.saveAvatarUrl(userId, avatarUrl)
--- src/service/file/file.ts
+++ src/service/file/file.ts
@@ -1,10 +1,33 @@
 import connection from '../../database'
 
 class fileservice {
-  async avatarInfo(filename: string, size: unknown, mimetype: string, userId: any) {
+  // 存储头像信息
+  async avatarInfo(filename: string, size: number, mimetype: string, userId: any) {
     try {
       const statement = `INSERT INTO avatar (fileName, size, mimetype, userId) VALUES (?, ?, ?, ?)`
       const [res] = await connection.execute(statement, [filename, size, mimetype, userId])
+      return res
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  // 判断下 avatar 表中有没有存储该用户的头像信息，没有存储的话才是插入，有的话更新
+  async isAvatarInfo(userId: number) {
+    try {
+      const statement = `SELECT * FROM avatar WHERE userId = ?`
+      const [res]: any = await connection.execute(statement, [userId])
+      return res.length ? true : false
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  // 当上传头像，用户原来上传过的
+  async updateAvatarInfo(filename: string, size: number, mimetype: string, userId: any) {
+    try {
+      const statement = `UPDATE avatar SET fileName = ?, size = ?, mimetype = ? WHERE userId = ?`
+      const [res] = await connection.execute(statement, [filename,size, mimetype, userId])
       return res
     } catch (error) {
       console.log(error)
